5b52ee510db1e76cd92328806bcd7bcb2bcdc0b0
Irving J. S. Linares <irjasali@gmail.com>
2022-04-25 00:44:59 -0500

    Versión final - Tecla Enter Activada

--- palindromo.js
+++ palindromo.js
@@ -1,30 +1,37 @@
-let cadenaOriginal =document.getElementById('cadena').value;
+let  keyP = document.getElementById('cadena');
 
-const Palindromo=()=> {
-    
+keyP.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+       Palindromo();
+    }
+});
 
-
-    if (cadenaOriginal === "") alert("Por favor introduzca una cadena valida");
-    str = cadenaOriginal.toLowerCase().replace(/[\W_]/g, '');
-    console.log(str)
-    let len = str.length - 1
-    console.log(len)
-    for(var i = 0; i < len/2; i++) {
-      console.log(str[i] + "!==" + str[len-i]);
-      if(str[i] !== str[len-i]) {
-        document.getElementById('resultado').innerText=" La cadena: ["+cadenaOriginal  +"] no es un palindromo";
-        return false ;
+const Palindromo = () => {
+  let cadenaOriginal = document.getElementById("cadena").value;
+  
+  if (cadenaOriginal === "") {
+    alert("Por favor introduzca una cadena valida");
+  } else {
+    str = cadenaOriginal.toLowerCase().replace(/[\W_]/g, "");
+    console.log(str);
+    let len = str.length - 1;
+    console.log(len);
+    for (var i = 0; i < len / 2; i++) {
+      console.log(str[i] + "!==" + str[len - i]);
+      if (str[i] !== str[len - i]) {
+        document.getElementById("resultado").innerText =
+          " La cadena: [" + cadenaOriginal + "] no es un palindromo";
+        return false;
       }
     }
-    document.getElementById('resultado').innerText= "La cadena: ["+cadenaOriginal  +"] si es un palindromo" ;
+    document.getElementById("resultado").innerText =
+      "La cadena: [" + cadenaOriginal + "] si es un palindromo";
     return true;
   }
-  
+}
 
-  const limpiar=()=>{
-    document.getElementById('resultado').innerHTML="";
-    document.getElementById('cadena').value="";
-
-
-  }
-  console.log(Palindromo("racecar"));
+const limpiar = () => {
+  document.getElementById("resultado").innerHTML = "";
+  document.getElementById("cadena").value = "";
+};
+// console.log(Palindromo("racecar"));
